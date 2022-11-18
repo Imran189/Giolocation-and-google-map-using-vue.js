4ec881a6be0a9a465d43c1de079c6a8b9a31229d
Imran189 <imranimu189@gmail.com>
2022-11-18 16:56:15 +0600

    giomap3

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import Vue3Geolocation from "vue3-geolocation";
 
-import './assets/main.css'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Vue3Geolocation);
+app.mount("#app");
