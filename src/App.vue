4f0946ff2250f266fc70ef7ef3894b0fcd133192
Imran189 <imranimu189@gmail.com>
2022-11-18 16:49:15 +0600

    giomap

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import mapViewVue from "./components/mapView.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
   </header>
 
   <main>
-    <TheWelcome />
+    <mapViewVue />
   </main>
 </template>
 
---
+++ src/components/mapView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref, reactive } from "vue";
+
+const coordinate = reactive({
+  lat: 0,
+  lag: 0,
+});
+</script>
+<template>
+  <div>
+    <h1>Your coordinates:</h1>
+    <p>{{ coordinate.lat }} Latitude, {{ coordinate.lag }} Longitude</p>
+  </div>
+</template>
